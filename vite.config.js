f99fda7b10e4c83dbe8690fc96731292af57a280
Aaron Hartono <aaronhartono28@gmail.com>
2025-03-29 22:15:42 +0700

    delete things

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/BRI/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
-});
+})
