32efaf196507ce291f6f225db86d885bfdd5d20f
Benedict Halim <benedicthalim1@gmail.com>
2025-04-01 10:12:51 +0700

    navbar mobile changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
-import { AppBar, Toolbar, Button as MuiButton, Stack, Icon, IconButton, Collapse } from "@mui/material";
+import { useState } from "react";
+import {
+    AppBar,
+    Toolbar,
+    Button as MuiButton,
+    Stack,
+    Icon,
+    IconButton,
+    Typography,
+    Collapse,
+    Button,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Drawer from "./Drawer";
 import Dropdown from "./Dropdown";
 import { Link } from "react-router-dom";
 
+import CottageIcon from "@mui/icons-material/Cottage";
 import SchoolIcon from "@mui/icons-material/School";
 import EventIcon from "@mui/icons-material/Event";
 import BookIcon from "@mui/icons-material/MenuBook";
+import RateReviewIcon from "@mui/icons-material/RateReview";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const menuItems = [
     { label: "Home", href: "/BRI/" },
@@ -15,9 +28,25 @@ const menuItems = [
     {
         label: "Study",
         dropdown: [
-            { label: "Kursus Online", desc: "Belajar di mana saja, kapan saja", icon: <SchoolIcon />, link: "/BRI/course" },
-            { label: "Webinar & Workshop", desc: "Belajar melalui event premium", icon: <EventIcon />, badge: "Rekomendasi", link: "/BRI/webinar" },
-            { label: "Alur Pembelajaran", desc: "Belajar secara terstruktur dengan silabus", icon: <BookIcon />, link: "/BRI/alur" },
+            {
+                label: "Kursus Online",
+                desc: "Belajar di mana saja, kapan saja",
+                icon: <SchoolIcon />,
+                link: "/BRI/course",
+            },
+            {
+                label: "Webinar & Workshop",
+                desc: "Belajar melalui event premium",
+                icon: <EventIcon />,
+                badge: "Rekomendasi",
+                link: "/BRI/webinar",
+            },
+            {
+                label: "Alur Pembelajaran",
+                desc: "Belajar secara terstruktur dengan silabus",
+                icon: <BookIcon />,
+                link: "/BRI/alur",
+            },
         ],
     },
 ];
@@ -26,11 +55,26 @@ const NavButtons = ({ sx }) => (
     <Stack direction="row" gap={2} sx={sx}>
         {menuItems.map(({ label, href, dropdown }) =>
             dropdown ? (
-                <Dropdown key={label} bgcolor="primary.main" color="#fff" bdcolor="#000" items={dropdown}>
+                <Dropdown
+                    key={label}
+                    bgcolor="primary.main"
+                    color="#fff"
+                    bdcolor="#000"
+                    items={dropdown}
+                >
                     {label}
                 </Dropdown>
             ) : (
-                <MuiButton key={label} color="inherit" component={Link} to={href} sx={{ textTransform: "capitalize", "&:hover": { color: "quaternary.main" } }}>
+                <MuiButton
+                    key={label}
+                    color="inherit"
+                    component={Link}
+                    to={href}
+                    sx={{
+                        textTransform: "capitalize",
+                        "&:hover": { color: "quaternary.main" },
+                    }}
+                >
                     {label}
                 </MuiButton>
             )
@@ -45,31 +89,173 @@ export default function Navbar() {
     return (
         <>
             {/* Mobile Drawer */}
-            <Drawer active={mobileOpen} setActive={setMobileOpen} title="Menu" bgcolor="primary.main" color="#fff" bdcolor="#000" list={menuItems}/>
+            <Drawer
+                active={mobileOpen}
+                setActive={setMobileOpen}
+                title="Menu"
+                bgcolor="primary.main"
+                color="#fff"
+                bdcolor="#000"
+                list={menuItems}
+            />
 
             {/* Top Navbar */}
-            <AppBar position="sticky" sx={{ bgcolor: "primary.main", boxShadow: "none", borderBottom: "3px solid", borderColor: "black_blue.main", color: "#fff" }}>
-                <Toolbar sx={{justifyContent:"space-between"}}>
+            <AppBar
+                position="sticky"
+                sx={{
+                    bgcolor: "primary.main",
+                    boxShadow: "none",
+                    borderBottom: "3px solid",
+                    borderColor: "black_blue.main",
+                    color: "#fff",
+                }}
+            >
+                <Toolbar sx={{ justifyContent: "space-between" }}>
                     {/* Mobile Menu Button */}
-                    <MuiButton color="#fff" bgcolor="primary.main" bdcolor="#000" onClick={() => setMobileOpen(true)} sx={{ display: { xs: "flex", sm: "none" }, p: 1 }}>
+                    <MuiButton
+                        color="#fff"
+                        bgcolor="primary.main"
+                        bdcolor="#000"
+                        onClick={() => setMobileOpen(true)}
+                        sx={{ display: { xs: "flex", sm: "none" }, p: 1 }}
+                    >
                         <MenuIcon />
                     </MuiButton>
 
-                    <Stack sx={{ display: { xs: "block", sm: "flex" }, flexGrow: {xs:0, sm:1} }} direction="row" justifyContent="space-around" alignItems="center">
+                    <Stack
+                        sx={{
+                            display: { xs: "block", sm: "flex" },
+                            flexGrow: { xs: 0, sm: 1 },
+                        }}
+                        direction="row"
+                        justifyContent="space-around"
+                        alignItems="center"
+                    >
                         {/* Logo */}
-                        <IconButton color="inherit" sx={{ p: "1vh 1.5vh", display: { xs: "none", sm: "block" }, }} >
+                        <IconButton
+                            color="inherit"
+                            sx={{
+                                p: "1vh 1.5vh",
+                                display: { xs: "none", sm: "block" },
+                            }}
+                        >
                             <Icon sx={{ height: "5vh" }}>
-                                <img src="/BRI/putih.svg" style={{ maxHeight: "5vh" }} alt="Rumah Ilmiah" />
+                                <img
+                                    src="/BRI/putih.svg"
+                                    style={{ maxHeight: "5vh" }}
+                                    alt="Rumah Ilmiah"
+                                />
                             </Icon>
                         </IconButton>
 
                         {/* Desktop Navigation */}
-                        <NavButtons sx={{ display: { xs: "none", sm: "block" } }} />
+                        <NavButtons
+                            sx={{ display: { xs: "none", sm: "block" } }}
+                        />
 
                         {/* Login Button */}
-                        <MuiButton color="inherit" variant="outlined" component={Link} sx={{ textTransform: "capitalize", p: "1vh 1.5vh" }} to="/BRI/login">
+                        <MuiButton
+                            color="inherit"
+                            variant="outlined"
+                            component={Link}
+                            sx={{ textTransform: "capitalize", p: "1vh 1.5vh" }}
+                            to="/BRI/login"
+                        >
                             Masuk/Daftar Akun
                         </MuiButton>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+
+            {/* Bottom Navbar */}
+            <AppBar
+                position="fixed"
+                color="primary"
+                sx={{
+                    top: "auto",
+                    bottom: 0,
+                    borderTop: "3px solid",
+                    borderColor: "kurai_ao.main",
+                    display: { xs: "flex", sm: "none" },
+                }}
+            >
+                <Toolbar>
+                    <Stack direction="row" gap={5}>
+                        <Button
+                            sx={{
+                                display: { xs: "flex", sm: "none" },
+                                p: 1,
+                                justifyContent: "center",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                            color="inherit"
+                            component={Link}
+                            to="/BRI/"
+                        >
+                            <RateReviewIcon color="white" />
+                            <Typography variant="caption">Blog</Typography>
+                        </Button>
+                        <Button
+                            sx={{
+                                display: { xs: "flex", sm: "none" },
+                                p: 1,
+                                justifyContent: "center",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                            color="inherit"
+                            component={Link}
+                            to="/BRI/course"
+                        >
+                            <SchoolIcon color="white" />
+                            <Typography variant="caption">Kursus</Typography>
+                        </Button>
+                        <Button
+                            sx={{
+                                display: { xs: "flex", sm: "none" },
+                                p: 1,
+                                justifyContent: "center",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                            color="inherit"
+                            component={Link}
+                            to="/BRI/"
+                        >
+                            <CottageIcon color="white" />
+                            <Typography variant="caption">Home</Typography>
+                        </Button>
+                        <Button
+                            sx={{
+                                display: { xs: "flex", sm: "none" },
+                                p: 1,
+                                justifyContent: "center",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                            color="inherit"
+                            component={Link}
+                            to="/BRI/"
+                        >
+                            <EventIcon color="white" />
+                            <Typography variant="caption">Webinar</Typography>
+                        </Button>
+                        <Button
+                            sx={{
+                                display: { xs: "flex", sm: "none" },
+                                p: 1,
+                                justifyContent: "center",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                            color="inherit"
+                            component={Link}
+                            to="/BRI/"
+                        >
+                            <AccountCircleIcon color="white" />
+                            <Typography variant="caption">Account</Typography>
+                        </Button>
                     </Stack>
                 </Toolbar>
             </AppBar>
