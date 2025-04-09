0ef8865f47a53ac2b807f80565f39138b7e72f2d
Aaron Hartono <aaronhartono28@gmail.com>
2025-04-10 01:14:02 +0700

    remove some unused things

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 // https://vite.dev/config/
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
 });
